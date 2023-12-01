76b09fc70973927399a93f1632c2ec224f24c3a9
LukasV2000 <lukasvysn@gmail.com>
2023-12-01 23:11:18 +0200

    Multifeature section

--- assets/custom_global.js
+++ assets/custom_global.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
+
+
+  $('.Index_personalization_feature').click(function(){
+    $('.Index_personalization_feature').removeClass('active');
+    $(this).addClass('active');
+    var data = $(this).attr('data');
+    $('.Index_personalization_image__js').hide();
+    $('.Index_personalization_image__js').each(function(){
+      if ($(this).attr('data') == data){
+        $(this).show();
+      }
+    });
+  });
+
+
   $('.Slideshow_outer').slick({
     infinite: true,
     slidesToShow:1,
