05bd990f7b6f58dfb7c9452c47e650aeb2dac1a8
LukasV2000 <lukasvysn@gmail.com>
2023-12-01 23:17:41 +0200

    specifying concrete section

--- assets/custom_global.js
+++ assets/custom_global.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
     $('.Index_personalization_feature').removeClass('active');
     $(this).addClass('active');
     var data = $(this).attr('data');
-    $('.Index_personalization_image__js').hide();
-    $('.Index_personalization_image__js').each(function(){
+    $(this).parent('div').find('.Index_personalization_image__js').hide();
+    $(this).parent('div').find('.Index_personalization_image__js').each(function(){
       if ($(this).attr('data') == data){
         $(this).show();
       }
