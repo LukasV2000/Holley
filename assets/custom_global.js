c33cf1d7296fbe3b59b5e0fd00a32f3e02acb075
LukasV2000 <lukasvysn@gmail.com>
2023-12-05 17:02:44 +0200

    Adding up reviews cell

--- assets/custom_global.js
+++ assets/custom_global.js
@@ -61,6 +61,23 @@ $(document).ready(function(){
     autoplaySpeed: 4000,
     pauseOnHover: false
   });
+
+  $('.Customer_says_reviews_outer').slick({
+    infinite: true,
+    slidesToShow:3,
+    slidesToScroll: 3,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow:1,
+          centerMode: true,
+          centerPadding: '30px',
+        }
+      }
+    ]
+  });
   				
   $('.Index_reviews_carousel').slick({
     infinite: true,
